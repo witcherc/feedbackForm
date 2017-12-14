5ad50a5b26344d0e848dcd97a07b43f1a0da62cd
witcherc <witcherc@gmail.com>
2017-12-14 15:29:19 -0800

    form preview tests

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,19 +1,41 @@
 jQuery(function($) {
-  var $fbEditor = $(document.getElementById("fb-editor")),
-      $formContainer = $(document.getElementById("fb-rendered-form")),
-      fbOptions = {
-        onSave: function() {
-          $fbEditor.toggle();
-          $formContainer.toggle();
-          $("form", $formContainer).formRender({
-            formData: formBuilder.formData
-          });
-        }
-      },
-      formBuilder = $fbEditor.formBuilder(fbOptions);
+  var $fbEditor = $(document.getElementById('fb-editor')),
+    $formContainer = $(document.getElementById('fb-rendered-form')),
+    fbOptions = {
+      editOnAdd: true,
+      onSave: function() {
+        $fbEditor.toggle();
+        $formContainer.toggle();
+        $('form', $formContainer).formRender({
+          formData: formBuilder.formData
+        });
+      }
+    },
+    formBuilder = $fbEditor.formBuilder(fbOptions);
 
-  $(".edit-form", $formContainer).click(function() {
+  $('.edit-form', $formContainer).click(function() {
     $fbEditor.toggle();
     $formContainer.toggle();
+  });
+
+  $('.preview-form', $formContainer).click(function() {
+    var hWndPreviewWindow =
+      window.open("", "Preview", "width=400,height=250,scrollbars=yes,resizable=yes, status=0");
+
+//find Field Name
+    var formDataNameIndex = formBuilder.formData.search("name");
+    var nameEndIndex = formBuilder.formData.indexOf(",", formDataNameIndex);
+    var nameValue = formBuilder.formData.substring(formDataNameIndex+7, nameEndIndex-1);
+
+//use Field Name to return field value
+    var fieldValue = formRender.formData;
+
+
+    hWndPreviewWindow.document.open();
+    hWndPreviewWindow.document.writeln('<HTML><HEAD><TITLE>Your Preview Window</TITLE></HEAD>');
+    hWndPreviewWindow.document.writeln('<BODY>')
+    hWndPreviewWindow.document.writeln('<B></B>');
+    hWndPreviewWindow.document.writeln(testForm.element);
+    hWndPreviewWindow.document.writeln('</BODY></HTML>');
   });
 });
