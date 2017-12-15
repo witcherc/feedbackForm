c299a4dfd3e0526b6e8172138bc0c41e96692a3c
witcherc <witcherc@gmail.com>
2017-12-14 23:02:14 -0800

    preview and some formatting

--- resources/js/index.js
+++ resources/js/index.js
@@ -2,6 +2,41 @@ jQuery(function($) {
   var $fbEditor = $(document.getElementById('fb-editor')),
     $formContainer = $(document.getElementById('fb-rendered-form')),
     fbOptions = {
+      disabledActionButtons: ['data'],
+
+      hiddenAttrs: ['name'],
+
+      disabledAttrs: [
+        'access',
+        'className',
+        'description',
+        'inline',
+        'label',
+        'max',
+        'maxlength',
+        'min',
+        'multiple',
+        'required',
+        'rows',
+        'step',
+        'style',
+        'subtype',
+        'toggle',
+        'value'
+      ],
+
+      disableFields: [
+        'button',
+        'file',
+        'hidden',
+        'header',
+        'date',
+        'number',
+        'autocomplete',
+        'select',
+        'paragraph'
+      ],
+
       editOnAdd: true,
       onSave: function() {
         $fbEditor.toggle();
@@ -11,31 +46,44 @@ jQuery(function($) {
         });
       }
     },
+    fields = [{
+      label: "Line Break",
+      type: "text",
+      icon: "<br>"
+    }],
+
     formBuilder = $fbEditor.formBuilder(fbOptions);
 
+
   $('.edit-form', $formContainer).click(function() {
+    document.getElementById("preview").innerHTML = "";
     $fbEditor.toggle();
     $formContainer.toggle();
   });
-
-  $('.preview-form', $formContainer).click(function() {
-    var hWndPreviewWindow =
-      window.open("", "Preview", "width=400,height=250,scrollbars=yes,resizable=yes, status=0");
-
-//find Field Name
-    var formDataNameIndex = formBuilder.formData.search("name");
-    var nameEndIndex = formBuilder.formData.indexOf(",", formDataNameIndex);
-    var nameValue = formBuilder.formData.substring(formDataNameIndex+7, nameEndIndex-1);
-
-//use Field Name to return field value
-    var fieldValue = formRender.formData;
-
-
-    hWndPreviewWindow.document.open();
-    hWndPreviewWindow.document.writeln('<HTML><HEAD><TITLE>Your Preview Window</TITLE></HEAD>');
-    hWndPreviewWindow.document.writeln('<BODY>')
-    hWndPreviewWindow.document.writeln('<B></B>');
-    hWndPreviewWindow.document.writeln(testForm.element);
-    hWndPreviewWindow.document.writeln('</BODY></HTML>');
-  });
 });
+
+
+function copyText() {
+  function selectElementText(el) {
+    var range = document.createRange() // create new range object
+    range.selectNodeContents(el) // set range to encompass desired element text
+    var selection = window.getSelection() // get Selection object from currently user selected text
+    selection.removeAllRanges() // unselect any user selected text (if any)
+    selection.addRange(range) // add range to Selection object to select it
+  };
+
+  var para = document.getElementById('preview');
+  selectElementText(para); // select the element's text we wish to read
+
+  function copySelectionText() {
+    var copysuccess // var to check whether execCommand successfully executed
+    try {
+      copysuccess = document.execCommand("copy") // run command to copy selected text to clipboard
+    } catch (e) {
+      copysuccess = false
+    }
+    return copysuccess;
+  };
+
+  copySelectionText(para);
+};
