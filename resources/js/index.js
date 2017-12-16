41ee171aacfdafcd08e070de637e64c9d31b1f67
witcherc <witcherc@gmail.com>
2017-12-16 10:03:03 -0800

    Switch to Skeleton.css and add some buttons

--- resources/js/index.js
+++ resources/js/index.js
@@ -49,18 +49,23 @@ jQuery(function($) {
 
     formBuilder = $fbEditor.formBuilder(fbOptions);
 
+  //function for edit form button: resets feedback preview and goes back to form editor
   $('.edit-form', $formContainer).click(function() {
     document.getElementById("preview").innerHTML = "";
     $fbEditor.toggle();
     $formContainer.toggle();
+    document.getElementById("copyText").style.display = "none";
   });
 });
 
+//function to clear the form
 function clearForm() {
   document.getElementById("testForm").reset();
   document.getElementById("preview").innerHTML = "";
-  };
+  document.getElementById("copyText").style.display = "none";
+};
 
+//function to copy the preview feedback to clipboard
 function copyText() {
   function selectElementText(el) {
     var range = document.createRange() // create new range object
@@ -87,11 +92,40 @@ function copyText() {
 };
 
 
-// When the user clicks on <div>, open the popup
+// When the user clicks on the button, open the popup
 function copyTooltipFunction() {
   var popup = document.getElementById("copyTooltip");
   popup.classList.toggle("show");
   setTimeout(function() {
     popup.classList.toggle("hide")
-  }, 3000);
+  }, 1000);
 };
+
+//function to save selected text to the bank
+function dndSave() {
+  function getSelectionText() {
+    var selectedText = "";
+    if (window.getSelection) { // all modern browsers and IE9+
+      selectedText = window.getSelection().toString();
+    }
+    return selectedText;
+  };
+
+  function saveToBank() {
+    var div = document.createElement("DIV");
+    var t = getSelectionText();
+    div.setAttribute("id", Date.now());
+    div.setAttribute("draggable", "true");
+    div.setAttribute("class", "bank");
+    div.innerHTML = t;
+    document.getElementById("previewContainer").appendChild(div);
+  };
+
+  saveToBank();
+};
+
+//listener to allow drag-and-drop of banked text
+
+document.addEventListener('dragstart', function(event) {
+  event.dataTransfer.setData('Text', event.target.innerHTML);
+});
